be711e1943f637eb81c2da7ed39225cfea6e6b24
mdevonshire <mdevonshire@silentcircle.com>
2017-11-07 22:36:19 -0500

    first commit

---
+++ scan.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(["netstat -an |grep 9200"])
---
+++ scripts.py
@@ -0,0 +1,5 @@
+import subprocess
+
+# subprocess.run("service kibana status")
+
+subprocess.run(["echo", "hello world"])
