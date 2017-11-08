a6eefa8d8e117f919075eb170b06a75b3e4d90ad
mdevonshire <mdevonshire@silentcircle.com>
2017-11-07 23:14:20 -0500

    scripts commit

--- scripts.py
+++ scripts.py
@@ -2,4 +2,17 @@ import subprocess
 
 # subprocess.run("service kibana status")
 
-subprocess.run(["echo", "hello world"])
+# subprocess.run(["echo", "hello "])
+
+def maisy(name, number):
+    if number == 4:
+        print("mika is a terrible dancer")
+    else:
+        subprocess.run(["echo", "hello "])
+
+
+def weston():
+    return maisy('mika', 4)
+
+
+weston()
