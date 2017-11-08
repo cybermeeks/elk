d130a3a0cabde423daf7df60f58183d58d3edbb6
mdevonshire <mdevonshire@silentcircle.com>
2017-11-07 23:16:56 -0500

    first scratch commit

--- socket.py
+++ socket.py
@@ -2,8 +2,7 @@ import socket
 import sys
 
 if len(sys.argv) != 2:
-    print
-    "Usage: vrfy.py <username>"
+    print("Usage: vrfy.py <username>")
     sys.exit(0)
 # Create a Socket
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Connect to the Server connect=s.connect(('10.11.1.215',25))
@@ -11,7 +10,8 @@ s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Connect to the Server conn
 banner=s.recv(1024)
 print(banner)
 # VRFY a user
-s.send('VRFY ' + sys.argv[1] + '\r\n') result=s.recv(1024)
-print result
+s.send('VRFY ' + sys.argv[1] + '\r\n')
+result=s.recv(1024)
+print(result)
 # Close the socket
 s.close()
